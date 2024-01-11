eb914cbc427c52d9176295d2c57acaaef72abb04
Lizaveta <kurash.lizaveta@gmail.com>
2024-01-11 18:16:06 +0100

    Add testConnection function to server/index.js

--- server/index.js
+++ server/index.js
@@ -34,3 +34,19 @@ const start = async()=>{
 };
 
 start();
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  } finally {
+    // Закрытие соединения
+    await sequelize.close();
+  }
+}
+
+testConnection();
