c0d84fb6ddebe266187ff392a501e15d42c4d1e5
Lizaveta <kurash.lizaveta@gmail.com>
2024-01-11 18:39:00 +0100

    Remove testConnection function from index.js

--- server/index.js
+++ server/index.js
@@ -34,19 +34,3 @@ const start = async()=>{
 };
 
 start();
-
-async function testConnection() {
-  try {
-    await sequelize.authenticate();
-    console.log(
-      "Connection to the database has been established successfully."
-    );
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  } finally {
-    // Закрытие соединения
-    await sequelize.close();
-  }
-}
-
-testConnection();
